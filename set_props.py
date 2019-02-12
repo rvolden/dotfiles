aaf5a65f987c46bce1ca188726798a1380e1be03
Roger Volden <rvolden@ucsc.edu>
2019-02-12 15:51:48 -0800

    Create set_props.py

---
+++ set_props.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+
+xinput_list = '/home/rvolden/.config/i3/inputs'
+os.system('xinput list >{0}'.format(xinput_list))
+for line in open(xinput_list):
+    line = line.rstrip()
+    if 'Logitech' in line and 'pointer' in line:
+        id = line.split('\t')[1][3:]
+        os.system('xinput set-prop {0} "libinput Natural Scrolling Enabled" 0'.format(id))
